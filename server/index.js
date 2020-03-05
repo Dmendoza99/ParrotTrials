db55de71049495a7476db78c800194df11e90980
David Mendoza <d.mendoza99@unitec.edu>
2020-03-04 23:33:47 -0600

    first commit, made backend and frontend structure

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
+var cors = require("cors");
+const PORT = 3001;
+const router = express.Router();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+router.post("/ping", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ message: "pong" });
+});
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`app listening on ${PORT}`);
+});

268731830865aec891a2444e9aa619fe962ec31e
David Mendoza <d.mendoza99@unitec.edu>
2020-03-07 08:28:19 -0600

    paths

--- server/updateData.sh
+++ server/updateData.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 LATEST_API="https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
 HISTORICAL_API="https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
-LATEST_PATH="./latestData.xml"
-HISTORICAL_PATH="./historicalData.xml"
+LATEST_PATH="~/ParrotTrials/server/latestData.xml"
+HISTORICAL_PATH="~/ParrotTrials/server/historicalData.xml"
 
 
 curl $LATEST_API -o $LATEST_PATH

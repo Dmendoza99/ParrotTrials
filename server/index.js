f10fcbe1fc0b1e237d36af496692e9d89bc68d22
David Mendoza <d.mendoza99@unitec.edu>
2020-03-07 17:09:25 -0600

    logger

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+const fs = require("fs");
 const latest = require("./routers/latest");
 const historical = require("./routers/historical");
 const PORT = 3001;
@@ -9,6 +12,8 @@ const PORT = 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
+app.use(morgan("tiny", { stream: accessLogStream }));
 
 app.use("/latest", latest);
 app.use("/historical", historical);

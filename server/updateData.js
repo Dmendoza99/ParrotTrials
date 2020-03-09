1847fd879b95e4d64228c9a77093697a8b122942
David Mendoza <d.mendoza99@unitec.edu>
2020-03-09 17:39:04 -0600

    update the cronjob

--- server/updateData.js
+++ server/updateData.js
@@ -2,8 +2,8 @@ const fetch = require("node-fetch");
 const fs = require("fs");
 const LATEST_API = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
 const HISTORICAL_API = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml";
-const LATEST_PATH = "./latestData.xml";
-const HISTORICAL_PATH = "./historicalData.xml";
+const LATEST_PATH = __dirname + "/latestData.xml";
+const HISTORICAL_PATH = __dirname + "/historicalData.xml";
 
 fetch(LATEST_API)
   .then(data => data.text())
